47d68dae04e1ea2ce1f7fcd3584bfeb5ee128f4a
HyeRyeong <fud3725@naver.com>
2022-04-18 11:13:28 +0900

    분가

---
+++ 0418/daynight.js
@@ -0,0 +1,18 @@
+function night(){
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('body').style.color = 'white';
+    $('body').css('color', 'white');
+    let temp = document.querySelectorAll('a');
+    for(let i = 0; i<temp.length; i++){
+        temp[i].style.color = 'white';
+    }
+}
+
+function day(){
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('body').style.color = 'black';
+    let temp = document.querySelectorAll('a');
+    for(let i = 0; i<temp.length; i++){
+        temp[i].style.color = 'black';
+    }
+}
